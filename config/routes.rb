c71d90990b9986a4c483ffedeb1cc09f3205aad9
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-27 15:36:00 -0600

    edit devise_for route to use our extended registrations controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   # You can have the root of your site routed with "root"
   root 'site#index'
