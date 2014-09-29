c605d14c9245b02377be07a849f1ca70fe870c26
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-29 14:40:21 -0600

    add some helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(user)
+    if user.staff?
+      staff_path
+    else
+      dashboard_path
+    end
+  end
+
+  protected
+
+  def alert_and_render(message, action)
+    flash[:alert] = message
+    render action
+  end
 end
