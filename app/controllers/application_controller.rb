938aa8146b92c401bce90912819c1958f8bce561
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-29 15:42:58 -0600

    add layout method to application controller to render public layout for devise controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  
+  layout :layout_by_resource
 
   def after_sign_in_path_for(user)
     if user.staff?
@@ -17,4 +19,13 @@ class ApplicationController < ActionController::Base
     flash[:alert] = message
     render action
   end
+
+  def layout_by_resource
+    if devise_controller?
+      'public'
+    else
+      'application'
+    end
+  end
+
 end
