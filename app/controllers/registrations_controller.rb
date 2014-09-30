b154cb03dfbe150a72773ad375ea8b30814c49ef
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-30 15:24:22 -0600

    update registrations controller to allow first and last name

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,14 +2,16 @@ class RegistrationsController < Devise::RegistrationsController
   private
 
   def sign_up_params
-    params.require(:user).permit(:name,
+    params.require(:user).permit(:first_name,
+                                 :last_name,
                                  :email,
                                  :password,
                                  :password_confirmation)
   end
 
   def account_update_params
-    params.require(:user).permit(:name,
+    params.require(:user).permit(:first_name,
+                                 :last_name,
                                  :email,
                                  :github_username,
                                  :hometown,
