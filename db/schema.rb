e05e05f2f7c38d8d937d4605165d246f68a1a83a
Ryan LeViseur <vidkun@vlfservices.com>
2014-09-30 11:48:14 -0600

    rake db migrate to add role to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140929205427) do
+ActiveRecord::Schema.define(version: 20140930174633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,8 +50,11 @@ ActiveRecord::Schema.define(version: 20140929205427) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "cohort_id"
+    t.string   "name"
+    t.integer  "role",                   default: 0
   end
 
+  add_index "users", ["cohort_id", "role"], name: "index_users_on_cohort_id_and_role", using: :btree
   add_index "users", ["cohort_id"], name: "index_users_on_cohort_id", using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
