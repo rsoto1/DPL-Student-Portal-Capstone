3d8bf84ac1b6dc1e9b188251d9e2b1589210af32
Rocio <rsoto1@hotmail.com>
2014-10-02 14:00:27 -0600

    Added notification validations for presence of user, email and sender email/name

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,3 +1,9 @@
 class Notification < ActiveRecord::
 	belongs_to :user
+
+	validates :user_id, presence: :true
+  validates :message, presence: :true
+  validates :sender_email, presence: :true
+  validates :sender_name, presence: :true
+  
 end
