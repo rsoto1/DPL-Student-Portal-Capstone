05b75c3f311f06fcfaac9e4324ae67c1b2503e86
Rocio <rsoto1@hotmail.com>
2014-10-02 13:55:01 -0600

    Added user associations for attendance,announcements and notifications

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :attendances, dependent: :destroy
+  has_many :announcements, dependent: :destroy
+  has_many :notifications, dependent: :destroy
   belongs_to :cohort
 
   enum role: [:student, :staff]
