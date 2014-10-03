dd21a309ac9c8bcd51fcb1aaa432d0c9291e740a
Rocio <rsoto1@hotmail.com>
2014-10-02 20:56:27 -0600

    Fixed indentation? Combined validations

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,9 +1,6 @@
 class Notification < ActiveRecord::
 	belongs_to :user
 
-	validates :user_id, presence: :true
-  validates :message, presence: :true
-  validates :sender_email, presence: :true
-  validates :sender_name, presence: :true
-  
+	validates :user_id, :message, :sender_email, :sender_name, presence: :true 
+ 
 end
