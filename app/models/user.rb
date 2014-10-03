c610772432c27d1feae5f70a8ced9899c19d425c
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-02 21:01:35 -0600

    add methods for creating new students to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,21 @@ class User < ActiveRecord::Base
   belongs_to :cohort
 
   enum role: [:student, :staff]
+
+  def self.build_with_temp_password(attributes = {})
+    token = Devise.friendly_token
+    attributes.merge!(temp_password: token,
+                      password: token,
+                      password_confirmation: token)
+    new(attributes)
+  end
+
+  def update_info(new_attributes)
+    new_attributes.merge!(temp_password: nil)
+    if new_attributes[:password].blank?
+      update_without_password(new_attributes)
+    else
+      update_attributes(new_attributes)
+    end
+  end
 end
