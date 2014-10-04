3bfa75ca499a88b873d6586cfa8cd08508dde033
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-04 13:45:19 -0600

    add student deletion capability for staff

--- app/controllers/staff/users_controller.rb
+++ app/controllers/staff/users_controller.rb
@@ -3,6 +3,7 @@ class Staff::UsersController < ApplicationController
   before_action :authenticate_user!
   before_action :ensure_staff!, only: [:new, :create]
   before_action :set_cohort
+  before_action :set_student, only: [:edit, :update, :destroy]
 
   def index
   end
@@ -18,8 +19,7 @@ class Staff::UsersController < ApplicationController
     @student = @cohort.users.build_with_temp_password(user_params)
 
     if @student.save
-      # redirect_to staff_cohort_users_path(@cohort), notice: 'Student created!'
-      redirect_to staff_path, notice: 'Student created!'
+      redirect_to staff_cohort_path(@cohort), notice: 'Student created!'
     else
       alert_and_render('Could not add new student', :new)
     end
@@ -34,11 +34,20 @@ class Staff::UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @student.destroy
+    redirect_to staff_cohort_path(@cohort), notice: 'Student was deleted'
+  end
+
   def profile
     @user = current_user
   end
 
   private
+
+  def set_student
+    @student = @cohort.users.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:first_name,
