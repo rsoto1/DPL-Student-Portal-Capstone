36ab6c2e5a53ca5cc12ee63c090eaca8373399d9
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-04 13:33:39 -0600

    add ability for staff to add students to a cohort

--- app/controllers/staff/users_controller.rb
+++ app/controllers/staff/users_controller.rb
@@ -1,11 +1,28 @@
 class Staff::UsersController < ApplicationController
+  include SetCohort
   before_action :authenticate_user!
   before_action :ensure_staff!, only: [:new, :create]
+  before_action :set_cohort
 
   def index
   end
 
   def edit
+  end
+
+  def new
+    @student = @cohort.users.build_with_temp_password
+  end
+
+  def create
+    @student = @cohort.users.build_with_temp_password(user_params)
+
+    if @student.save
+      # redirect_to staff_cohort_users_path(@cohort), notice: 'Student created!'
+      redirect_to staff_path, notice: 'Student created!'
+    else
+      alert_and_render('Could not add new student', :new)
+    end
   end
 
   def update
