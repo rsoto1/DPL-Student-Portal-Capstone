2e6d03b8890220043cf245dc8f56b8d19e8019a5
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-06 16:09:09 -0600

    add omniauthable github to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  devise :omniauthable, :omniauth_providers => [:github]
   belongs_to :cohort
   has_many :attendances, dependent: :destroy
   has_many :announcements, dependent: :destroy
