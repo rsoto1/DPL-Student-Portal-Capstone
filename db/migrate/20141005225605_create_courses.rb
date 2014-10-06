6dc1ff09c2ff4c5f607d4df46387f5fe5a0ddec3
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-05 20:50:47 -0600

    update migration to change column type and add new column

--- db/migrate/20141005225605_create_courses.rb
+++ db/migrate/20141005225605_create_courses.rb
@@ -3,7 +3,8 @@ class CreateCourses < ActiveRecord::Migration
     create_table :courses do |t|
       t.string :name
       t.text :description
-      t.string :duration
+      t.integer :duration
+      t.string :duration_unit
       t.decimal :price, precision: 8, scale: 2
 
       t.timestamps
