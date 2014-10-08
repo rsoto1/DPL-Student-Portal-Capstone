de0466b56e5c118ef62fe57da522764e8d62f417
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-08 15:26:57 -0600

    update user model omniauth method to check github_email for match as well

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,7 +41,8 @@ class User < ActiveRecord::Base
   # end
 
   def self.get_and_update_from_omniauth(auth)
-    user = where("email = ? OR provider = ? AND uid = ? ",
+    user = where("email = ? OR github_email = ? OR provider = ? AND uid = ? ",
+                 auth.info.email,
                  auth.info.email,
                  auth.provider,
                  auth.uid).first
@@ -50,6 +51,8 @@ class User < ActiveRecord::Base
     user.github_username = auth.extra.raw_info.login
     user.github_access_token = auth.credentials.token
     user.github_state = 'completed'
+    user.provider = auth.provider
+    user.uid = auth.uid
     user
     # user.email = auth.info.email
     # user.password = Devise.friendly_token[0,20]
