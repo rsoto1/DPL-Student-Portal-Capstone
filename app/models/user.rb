bd8e80890c6854435559167be9c5bde4d570607a
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-10 20:10:41 -0600

    Update User model for GitHub API usage * Update from_omniauth method to only return the user (used   for signing in with GitHub) * Add new create_github_profile method to handle building   a github_profile object that is associated with a user   upon linking their github account to their user account * Add methods for using the github API to link and authorize   user accounts (generating github state, setting attributes   for authorization, and checking if authorized)

--- app/models/user.rb
+++ app/models/user.rb
@@ -41,20 +41,53 @@ class User < ActiveRecord::Base
   #   end
   # end
 
-  def self.get_and_update_from_omniauth(auth)
-    user = where("email = ? OR github_email = ? OR provider = ? AND uid = ? ",
-                 auth.info.email,
-                 auth.info.email,
-                 auth.provider,
-                 auth.uid).first
-    user.github_username = auth.extra.raw_info.login
-    user.github_access_token = auth.credentials.token
-    user.github_state = 'completed'
-    user.provider = auth.provider
-    user.uid = auth.uid
-    user
-    # user.email = auth.info.email
-    # user.password = Devise.friendly_token[0,20]
-    # user.image = auth.info.image # assuming the user model has an image
+  def self.from_omniauth(auth)
+    where('provider = ? AND uid = ?',
+          auth.provider,
+          auth.uid).first
+  end
+
+  def create_github_profile
+    client = Octokit::Client.new access_token: github_access_token
+    user = client.user
+    self.update_attributes(uid: user.id)
+    GithubProfile.create(user_id: id,
+                         username: user.login || '',
+                         email: client.emails.select { |el| el['primary'] }.first.email || '',
+                         name: user.name || '',
+                         image: user.avatar_url || '',
+                         public_repo: user.public_repos || '',
+                         public_gists: user.public_gists || '',
+                         member_since: user.created_at || '',
+                         access_token: client.access_token || '')
+    # GithubProfile.create(user_id: id,
+    #                      username: auth.info.nickname || '',
+    #                      email: auth.info.email || '',
+    #                      name: auth.info.name || '',
+    #                      image: auth.info.image || '',
+    #                      location: auth.extra.raw_info.location || '',
+    #                      public_repo: auth.extra.raw_info.public_repos || '',
+    #                      public_gists: auth.extra.raw_info.public_gists || '',
+    #                      member_since: auth.extra.raw_info.created_at || '',
+    #                      access_token: auth.credentials.token)
+  end
+
+  def generate_github_state!
+    self.update_attributes(github_state: self.generate_github_state)
+    self.github_state
+  end
+
+  def generate_github_state
+    SecureRandom.hex
+  end
+
+  def authorized_for_github!(authorized_token)
+    self.update_attributes(github_access_token: authorized_token,
+                           github_state: 'linked',
+                           provider: 'github')
+  end
+
+  def github_authorized?
+    self[:github_access_token] && self[:github_state] == 'linked'
   end
 end
