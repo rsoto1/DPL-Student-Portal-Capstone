9dd6487c36335ba975e02e03ae02cb089bfba852
Rocio <rsoto1@hotmail.com>
2014-10-11 16:23:28 -0600

    Courses modal working

--- app/controllers/staff/base_controller.rb
+++ app/controllers/staff/base_controller.rb
@@ -2,6 +2,7 @@ class Staff::BaseController < ApplicationController
   before_action :authenticate_user!
   def index
     @cohorts = Cohort.all
+    @cohort = Cohort.new
   end
   def admin
     @location = Location.new
