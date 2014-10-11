7b0411678abf673f95d4c568499cbef5d8d54464
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-10 21:15:36 -0600

    add empty methods to github_webhooks controller to be used for processing events

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,2 +1,17 @@
 class GithubWebhooksController < ApplicationController
+  include GithubWebhook::Processor
+
+  def push(payload)
+    # TODO: handle push webhook
+  end
+
+  def pull_request(payload)
+  end
+
+  def pull_request_review_comment(payload)
+  end
+
+  def webhook_secret(payload)
+    ENV['GITHUB_WEBHOOK_SECRET']
+  end
 end
