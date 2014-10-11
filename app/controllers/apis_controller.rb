7b0878779dd1766599b32cf86c14b3491ba45acd
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-10 20:02:18 -0600

    uncomment and update github_callback method to handle linking github account to user account

--- app/controllers/apis_controller.rb
+++ app/controllers/apis_controller.rb
@@ -1,13 +1,14 @@
 class ApisController < Devise::OmniauthCallbacksController
-  # def github_callback
-  #   user = User.find_by(github_state: params[:state]) if params[:state]
-  #   redirect_to root_path, notice: 'Invalid flow. Something is not right' unless user
-  #   access_token = Apis::Github.get_access_token(user, params[:code])
-  #   redirect_to root_path, notice: 'Invalid flow. Something is not right, could not get access token' unless access_token
-  #   user.authorized_for_github!(access_token)
-  #   user.update_attribute(:github_username, Apis::Github.get_user_information(user))
-  #   redirect_to user_path(user)
-  # end
+  include Apis
+  def github_callback
+    user = User.find_by(github_state: params[:state]) if params[:state]
+    redirect_to root_path, notice: 'Invalid flow. Something is not right' unless user
+    access_token = Apis::Github.get_access_token(user, params[:code])
+    redirect_to root_path, notice: 'Invalid flow. Something is not right, could not get access token' unless access_token
+    user.authorized_for_github!(access_token)
+    user.create_github_profile
+    redirect_to dashboard_profile_path
+  end
 
   def github
     # Implement the method below in your model (e.g. app/models/user.rb)
