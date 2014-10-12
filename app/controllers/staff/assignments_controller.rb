3883d9f37606e9b5a6542e11a7e2f56d4b337ed7
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-11 22:07:26 -0600

    update paths in staff/assignments controller and actions with proper methods

--- app/controllers/staff/assignments_controller.rb
+++ app/controllers/staff/assignments_controller.rb
@@ -18,7 +18,7 @@ class Staff::AssignmentsController < ApplicationController
   def update
     respond_to do |format|
       if @assignment.update(assignment_params)
-        format.html { redirect_to staff_assignments_url, notice: 'Assignment was successfully updated.' }
+        format.html { redirect_to staff_cohort_assignments_url, notice: 'Assignment was successfully updated.' }
         format.json { render :show, status: :ok, location: @assignment }
       else
         format.html { render :edit }
@@ -28,14 +28,15 @@ class Staff::AssignmentsController < ApplicationController
   end
 
   def new
+    @assignment = Assignment.new
   end
 
   def create
-    @assignment = @cohort.assignment.build(assignment_params)
+    @assignment = @cohort.assignments.build(assignment_params)
 
     respond_to do |format|
       if @assignment.save
-        format.html { redirect_to staff_assignments_url, notice: 'Assignment was successfully created.' }
+        format.html { redirect_to staff_cohort_assignments_url, notice: 'Assignment was successfully created.' }
         format.json { render :show, status: :created, location: @assignment }
       else
         format.html { render :new }
@@ -52,7 +53,7 @@ class Staff::AssignmentsController < ApplicationController
       flash[:notice] = e.message
     end
     respond_to do |format|
-      format.html { redirect_to staff_assignments_url }
+      format.html { redirect_to staff_cohort_assignments_url }
       format.json { head :no_content }
     end
   end
