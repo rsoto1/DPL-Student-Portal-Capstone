0dee877ca62be8069becc2ef9f977262cbb2c6bd
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-12 00:01:29 -0600

    add helper method for setting a page header to ease conflicts with title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,10 @@ module ApplicationHelper
     content_for(:title) { page_title }
   end
 
+  def page_header(page_header)
+    content_for(:page_header) { page_header }
+  end
+
   def get_has_many_assocations(format = false, options={})
     if format
       ul(AppStats::Stats.get_has_many_relationships, options)
