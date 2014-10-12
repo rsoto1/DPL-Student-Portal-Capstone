cf2b0ab7c4823e775d6f821bff3232ca91dcb358
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-11 22:54:13 -0600

    migrate db to add answers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141012040950) do
+ActiveRecord::Schema.define(version: 20141012045209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,18 @@ ActiveRecord::Schema.define(version: 20141012040950) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "answers", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "assignment_id"
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "answers", ["assignment_id"], name: "index_answers_on_assignment_id", using: :btree
+  add_index "answers", ["user_id"], name: "index_answers_on_user_id", using: :btree
 
   create_table "assignments", force: true do |t|
     t.string   "name"
