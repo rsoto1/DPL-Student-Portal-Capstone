60385fb24b91fe42af5fc9682ff062e345e24a07
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-18 17:08:25 -0600

    update staff/answers controller with method to handle ajax query for updating accepted attribute

--- app/controllers/staff/answers_controller.rb
+++ app/controllers/staff/answers_controller.rb
@@ -1,2 +1,23 @@
 class Staff::AnswersController < ApplicationController
+  before_action :authenticate_user!
+
+  def update_answer_acceptance
+    @answer = Answer.find(params[:answer_id])
+    checked = params[:accepted] || false
+    @answer.update_attribute(:accepted, checked)
+    respond_to do |format|
+      format.all { render nothing: true, status: 200 }
+    end
+  end
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:title,
+                                   :body,
+                                   :accepted,
+                                   :assignment_id,
+                                   :user_id,
+                                   :answer_id)
+  end
 end
