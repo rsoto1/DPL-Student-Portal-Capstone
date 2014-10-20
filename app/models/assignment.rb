5961c20eaf03736fed5e9364773ad9e8c47a2d12
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-19 20:21:07 -0600

    Update assignment model * Add method for calculating percentage of students that have   submitted an answer for an assignment * Add method for returning all answers to an assignment that   have been marked as accepted * Add method for calculating percentage of students have an   accepted answer for an assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -3,4 +3,16 @@ class Assignment < ActiveRecord::Base
   has_many :pull_requests
   has_many :answers, dependent: :destroy
   validates :name, :description, presence: :true
+
+  def accepted_answers
+    answers.where(accepted: true)
+  end
+
+  def percent_answers_accepted
+    100.0 * (accepted_answers.count.to_f / cohort.users.student.count)
+  end
+
+  def percent_answered
+    100.0 * (answers.count.to_f / cohort.users.student.count)
+  end
 end
