f104b6dd4cebc5e9d83098bdcd49d9f68f0ae356
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-21 14:33:26 -0600

    update members controller to send off welcome mailer upon new staff creation

--- app/controllers/staff/members_controller.rb
+++ app/controllers/staff/members_controller.rb
@@ -17,6 +17,7 @@ class Staff::MembersController < ApplicationController
     @member = User.build_with_temp_password(member_params)
     @member.staff!
     if @member.save
+      StudentMailer.welcome_email(@member).deliver
       redirect_to staff_base_admin_path, notice: 'Welcome to the family!'
     else
       alert_and_render('Could not save admin info', :new)
