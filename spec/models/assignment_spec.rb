0b215720dd906368155197584592695f637f728a
Rocio <rsoto1@hotmail.com>
2014-10-21 18:13:53 -0600

    Fix url for mailer

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
+require 'pry'
 
 RSpec.describe Assignment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @assignment = FactoryGirl.build(:assignment)
+  end
+
+  it "should be invalid if it has no name" do
+    @assignment.name = nil
+    @assignment.should_not be_valid
+  end
+
 end
