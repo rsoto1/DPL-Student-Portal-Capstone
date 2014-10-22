070b389ae13cf7ea72424b30d54d099fc33a1c36
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-22 15:07:13 -0600

    add some tests to assignment spec, but one is still failing

--- spec/models/assignment_spec.rb
+++ spec/models/assignment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Assignment, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should belong_to :cohort }
+    it { should have_many :pull_requests }
+    it { should have_many :answers }
+  end
+
+  describe 'validates name' do
+    it { should validate_presence_of :name }
+  end
+
+  describe '#accepted_answers' do
+    before do
+      @location = create(:location)
+      @cohort = create(:cohort,
+                       location: @location)
+      @assignment = create(:assignment,
+                           cohort: @cohort)
+      @accepted_answers = create_list(:answer,
+                                      4,
+                                      assignment: @assignment,
+                                      accepted: true)
+      @unaccepted_answers = create_list(:answer,
+                                        4,
+                                        assignment: @assignment)
+    end
+    it 'returns 4' do
+      expect(@assignment.accepted_answers).to eq 4
+    end
+  end
 end
