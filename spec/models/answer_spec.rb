b7539d00dd4134c6986f0d96ecc34bca5ee67b47
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-22 11:56:01 -0600

    add initial test to answer model

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Answer, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_one :pull_request }
+    it { should belong_to :user }
+    it { should belong_to :assignment }
+  end
+
+  # not working at the moment. need to build an assignment to test against
+  # describe 'validates only one answer per user for each assignment' do
+  #   it { should validate :users_only_answer }
+  # end
+
+  # describe 'validates title and body' do
+  #   it { should validate_presence_of :title }
+  #   it { should validate_presence_of :body }
+  # end
 end
