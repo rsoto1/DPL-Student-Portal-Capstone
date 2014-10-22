9e25e010a05e44b4b0ad3ccb42fe2a01b09b7f6c
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-22 15:25:36 -0600

    update users factory with faker data

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,15 +1,23 @@
 FactoryGirl.define do
   # Build a student user
   factory :student do
-    first_name 'Bill'
-    last_name 'Preston'
-    role student
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.safe_email }
+    password { 'password' }
+    password_confirmation { 'password' }
+    temp_password { 'password' }
+    role { :student }
   end
 
   # build a staff user
   factory :staff, class: User do
-    first_name 'Ted'
-    last_name 'Logan'
-    role staff
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    email { Faker::Internet.safe_email }
+    password { 'password' }
+    password_confirmation { 'password' }
+    temp_password { 'password' }
+    role { :staff }
   end
 end
