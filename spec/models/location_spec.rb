9ab0f84265b48f35ab62285a9430bd5a1f83abe9
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-22 11:45:11 -0600

    add tests for location model

--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Location, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { should have_many :cohorts }
+  end
+
+  describe 'validates name' do
+    it { should validate_presence_of :name }
+  end
 end
