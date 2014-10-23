4d52876e9a80b43d86c00344f5199e28680a2e29
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-22 18:54:27 -0600

    update users factory to use Faker data and have a student and staff model

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,23 +1,26 @@
 FactoryGirl.define do
-  # Build a student user
-  factory :student do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.safe_email }
-    password { 'password' }
-    password_confirmation { 'password' }
-    temp_password { 'password' }
-    role { :student }
-  end
+  factory :user do
+    # Build a student user
+    factory :student, class: User do
+      first_name { Faker::Name.first_name }
+      last_name { Faker::Name.last_name }
+      email { Faker::Internet.safe_email }
+      password { 'password' }
+      password_confirmation { 'password' }
+      temp_password { 'password' }
+      role { :student }
+      cohort
+    end
 
-  # build a staff user
-  factory :staff, class: User do
-    first_name { Faker::Name.first_name }
-    last_name { Faker::Name.last_name }
-    email { Faker::Internet.safe_email }
-    password { 'password' }
-    password_confirmation { 'password' }
-    temp_password { 'password' }
-    role { :staff }
+    # build a staff user
+    factory :staff, class: User do
+      first_name { Faker::Name.first_name }
+      last_name { Faker::Name.last_name }
+      email { Faker::Internet.safe_email }
+      password { 'password' }
+      password_confirmation { 'password' }
+      temp_password { 'password' }
+      role { :staff }
+    end
   end
 end
