9e0030134c6d2e6130ef82ae6d22c6084a8c34c3
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-23 19:03:51 -0600

    add tests for welcome_email mailer

--- spec/mailers/student_mailer_spec.rb
+++ spec/mailers/student_mailer_spec.rb
@@ -1,5 +1,28 @@
 require "rails_helper"
 
 RSpec.describe StudentMailer, :type => :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'welcome_email' do
+    let(:user) { create :student }
+    let(:mail) { StudentMailer.welcome_email(user) }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eql('Welcome to the Student Portal')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eql([user.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eql(['staff@example.com'])
+    end
+
+    it 'assigns @url' do
+      expect(mail.body.encoded).to match('http://dpl-student-portal-capstone.herokuapp.com')
+    end
+
+    it "includes user's temp password" do
+      expect(mail.body.encoded).to match(user.temp_password)
+    end
+  end
 end
