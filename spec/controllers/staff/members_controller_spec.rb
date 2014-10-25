cfe5e087fe46556f11e88fbd6764eb6b85a09f77
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-24 22:25:36 -0600

    add tests for staff members controller

--- spec/controllers/staff/members_controller_spec.rb
+++ spec/controllers/staff/members_controller_spec.rb
@@ -1,54 +1,81 @@
 require 'rails_helper'
 
 RSpec.describe Staff::MembersController, :type => :controller do
+  before do
+    # @location = create(:location)
+    # @cohort = create(:cohort, location: @location)
+    @staff = create(:staff)
+    sign_in(@staff)
+  end
 
-  describe "GET index" do
-    it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+  describe '#show' do
+    before do
+      @member = create(:staff,
+                       first_name: 'Some',
+                       last_name: 'Staff')
+    end
+    it 'renders info about a staff member' do
+      get :show, id: @member.id
+      expect(response).to be_success
+      expect(@member.first_name).to eq 'Some'
+      expect(@member.last_name).to eq 'Staff'
+      expect(response).to render_template('show')
     end
   end
 
-  describe "GET show" do
-    it "returns http success" do
-      get :show
-      expect(response).to have_http_status(:success)
+  describe '#create' do
+    before do
+      @params = { user: { first_name: 'Staff',
+                          last_name: 'Name',
+                          email: 'email@mail.com',
+                          password: 'password',
+                          password_confirmation: 'password',
+                          role: :staff
+                        }
+                }
+    end
+
+    it 'saves the staff member' do
+      expect {
+        post :create, @params
+      }.to change(User, :count).by(1)
+    end
+
+    it 'sends an email' do
+      expect {
+        post :create, @params
+      }.to change { ActionMailer::Base.deliveries.count }.by(1)
     end
   end
 
-  describe "GET new" do
-    it "returns http success" do
-      get :new
-      expect(response).to have_http_status(:success)
+  describe '#update' do
+    before do
+      @staff = create(:staff)
+      @params = { id: @staff.id,
+                  user: { first_name: 'Changed',
+                          last_name: 'Name'
+                        }
+                }
+    end
+    it 'updates the staff member' do
+      patch :update, @params
+      assert_response :redirect
+      user = User.find(@staff.id)
+      expect(user.first_name).to eq 'Changed'
+      expect(user.last_name).to eq 'Name'
     end
   end
 
-  describe "GET create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+  describe '#destroy' do
+    before do
+      @staff = create(:staff)
+      @params = { id: @staff.id }
+    end
+
+    it 'destroys the staff member' do
+      expect {
+        delete :destroy, @params
+      }.to change(User, :count).by(-1)
     end
   end
-
-  describe "GET edit" do
-    it "returns http success" do
-      get :edit
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET update" do
-    it "returns http success" do
-      get :update
-      expect(response).to have_http_status(:success)
-    end
-  end
-
-  describe "GET destroy" do
-    it "returns http success" do
-      get :destroy
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
