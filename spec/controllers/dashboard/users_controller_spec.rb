6f3524e99bee7bcf1c882a3ff69d477e007c0c52
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-26 19:46:21 -0600

    add test for student profile to dashboard users controller spec

--- spec/controllers/dashboard/users_controller_spec.rb
+++ spec/controllers/dashboard/users_controller_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Dashboard::UsersController, :type => :controller do
+  before do
+    @location = create(:location)
+    @cohort = create(:cohort, location: @location)
+    @student = create(:student)
+    sign_in(@student)
+  end
 
+  describe '#profile' do
+    it 'renders info about a student' do
+      get :profile
+      expect(response).to be_success
+      expect(assigns(:user)).to eq(@student)
+      expect(response).to render_template('profile')
+    end
+  end
+
+  # describe '#update' do
+  #   before do
+  #     @student = create(:student, cohort: @cohort)
+  #     @params = { id: @student.id,
+  #                 cohort_id: @cohort.id,
+  #                 user: { first_name: 'Changed',
+  #                         last_name: 'Name' }
+  #               }
+  #   end
+  #   it 'updates the student' do
+  #     @student.update_info(@params)
+  #     assert_response :redirect
+  #     user = User.find(@student.id)
+  #     expect(user.first_name).to eq 'Changed'
+  #     expect(user.last_name).to eq 'Name'
+  #   end
+  # end
 end
