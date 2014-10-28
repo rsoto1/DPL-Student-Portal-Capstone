644ffa95c37bc2676b73a8485ba7b2331e8646f6
Ryan LeViseur <vidkun@vlfservices.com>
2014-10-28 14:58:33 -0600

    update seeds file to work in prod env

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
-if Rails.env.development?
+#
+if Rails.env.development? || Rails.env.production?
   seed_file = Rails.root.join('db', 'seeds.yml')
   seed_data = YAML::load_file(seed_file)
 
